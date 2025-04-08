75a24ea65ec27eae440788bf54e9bcc485a6f401
Carlos Lima <150730209+carlos-lima-dev@users.noreply.github.com>
2025-04-08 21:04:59 +0100

    Update appointments.service.ts

--- src/app/service/appointments.service.ts
+++ src/app/service/appointments.service.ts
@@ -7,7 +7,7 @@ import { Appointment } from '../interfaces/appointment';
   providedIn: 'root',
 })
 export class AppointmentsService {
-  private apiUrl = 'https://https://bookingapi-nqqx.onrender.com';
+  private apiUrl = 'https://https://bookingapi-nqqx.onrender.com/appointments';
 
   constructor(private http: HttpClient) {}
 
@@ -17,7 +17,7 @@ export class AppointmentsService {
   }
 
   bookAppointment(appointment: Appointment): Observable<any> {
-    return this.http.post(this.apiUrl/appointments, appointment);
+    return this.http.post(this.apiUrl, appointment);
   }
 
   deleteAppointment(appointmentId: string): Observable<void> {
